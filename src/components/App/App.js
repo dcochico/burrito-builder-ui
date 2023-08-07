02c1e37fdd845d5e4c81286f97f6a506c4546ef8
Daniel Cochico <dcochico.20@gmail.com>
2023-08-07 08:15:04 -0700

    feat: Add orders state and pass as prop

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { getOrders } from "../../apiCalls";
 import Orders from "../../components/Orders/Orders";
 import OrderForm from "../../components/OrderForm/OrderForm";
 
 const App = () => {
+  const [orders, setOrders] = useState([]);
+
   useEffect(() => {
     getOrders().catch((err) => console.error("Error fetching:", err));
   });
@@ -16,7 +18,7 @@ const App = () => {
         <OrderForm />
       </header>
 
-      <Orders orders={"Here is where orders go"} />
+      <Orders orders={orders} />
     </main>
   );
 }
