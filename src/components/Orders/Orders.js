878ad4fe9eac6d03fdd3b9eef4d1596089deb67e
Daniel Cochico <dcochico.20@gmail.com>
2023-08-07 11:00:11 -0700

    feat: Add unique keys

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,14 +1,17 @@
 import React from "react";
+import { nanoid } from "nanoid";
 import "./Orders.css";
 
 const Orders = ({ orders }) => {
   const orderEls = orders.map((order) => {
+    let orderKey = nanoid();
     return (
-      <div className="order">
+      <div key={orderKey} className="order">
         <h3>{order.name}</h3>
         <ul className="ingredient-list">
           {order.ingredients.map((ingredient) => {
-            return <li>{ingredient}</li>;
+            let ingredientKey = nanoid();
+            return <li key={ingredientKey} >{ingredient}</li>;
           })}
         </ul>
       </div>
