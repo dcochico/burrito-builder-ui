8e80eb3831ba924ece3922486c95dfbf555b638d
Daniel Cochico <dcochico.20@gmail.com>
2023-08-07 09:08:24 -0700

    feat: Write out logic for POST request

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,1 +1,16 @@
-export const getOrders = () => fetch("http://localhost:3001/api/v1/orders")
+const getOrders = () => fetch("http://localhost:3001/api/v1/orders")
+
+const postOrder = order => {
+  return fetch("http://localhost:3001/api/v1/orders", {
+    method: 'POST',
+    body: JSON.stringify({
+      name: order.name,
+      ingredients: order.ingredients
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+}
+
+export { getOrders, postOrder }
