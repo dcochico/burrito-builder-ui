c9b5399843ccabfbd59d605993fcd7b2c101f4e3
Daniel Cochico <dcochico.20@gmail.com>
2023-08-07 09:48:07 -0700

    feat: Add Cypress commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,18 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('stubRequest', (method, code, fixture) => {
+  return cy.intercept(method, "http://localhost:3001/api/v1/orders", {
+    statusCode: code,
+    fixture: fixture
+  })
+});
+
+Cypress.Commands.add('getData', () => {
+  cy.stubRequest('GET', 200, 'orders')
+});
+
+Cypress.Commands.add('dropData', () => {
+  cy.stubRequest('GET', 500, 'orders')
+});
+
+Cypress.Commands.add('postData', () => {
+  cy.stubRequest('POST', 201, 'newOrder')
+});
